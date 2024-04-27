cb3daa1b57f52d037631a0259fa83a4f6821f208
Ogunyemi Daniel <ogunyemidaniel65@gmail.com>
2024-04-27 04:50:12 -0700

    Status of your API

---
+++ api/v1/app.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""An api module"""
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+import os
+
+app = Flask(__name__)
+
+# Register blueprint
+app.register_blueprint(app_views)
+
+# Teardown app context to close storage
+@app.teardown_appcontext
+def teardown_appcontext(exception):
+    storage.close()
+
+
+if __name__ == "__main__":
+    # Run Flask server
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = os.getenv('HBNB_API_PORT', 5000)
+    app.run(host=host, port=port, threaded=True)
---
+++ api/v1/views/index.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""An index file"""
+# api/v1/views/index.py
+from api.v1.views import app_views
+from flask import jsonify
+
+# Define route
+@app_views.route('/status', methods=['GET'])
+def get_status():
+    """W method call when /status is accessed"""
+    return jsonify({"status": "OK"})
